f3f12ef01667c323643582157573a400c14bff64
Kai <klfn45@gmail.com>
2026-05-12 16:52:06 +0700

    Sending and processing event.

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,66 @@
 use borsh::{BorshDeserialize, BorshSerialize};
-use crosstown_bus::{CrosstownBus, HandleError, MessageHandler};
+use lapin::{
+    options::{BasicPublishOptions, QueueDeclareOptions},
+    types::FieldTable,
+    BasicProperties, Connection, ConnectionProperties,
+};
+
 #[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
 pub struct UserCreatedEventMessage {
     pub user_id: String,
     pub user_name: String,
 }
-pub struct UserCreatedHandler;
-impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
-    fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), HandleError> {
-        println!("Message received on handler 1: {:?}", message);
-        Ok(())
-    }
 
-    fn get_handler_action(&self) -> String {
-        "".to_string()
-    }
-}
-fn main() {
-    let mut p =
-        CrosstownBus::new_queue_publisher("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
-    _ = p.publish_event(
-        "user_created".to_owned(),
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let connection =
+        Connection::connect("amqp://guest:guest@127.0.0.1:5672", ConnectionProperties::default())
+            .await?;
+    let channel = connection.create_channel().await?;
+
+    channel
+        .queue_declare(
+            "user_created".into(),
+            QueueDeclareOptions::default(),
+            FieldTable::default(),
+        )
+        .await?;
+
+    for message in [
         UserCreatedEventMessage {
             user_id: "1".to_owned(),
             user_name: "2406360256-Amir".to_owned(),
         },
-    );
-    _ = p.publish_event(
-        "user_created".to_owned(),
         UserCreatedEventMessage {
             user_id: "2".to_owned(),
             user_name: "2406360256-Budi".to_owned(),
         },
-    );
-    _ = p.publish_event(
-        "user_created".to_owned(),
         UserCreatedEventMessage {
             user_id: "3".to_owned(),
             user_name: "2406360256-Cica".to_owned(),
         },
-    );
-    _ = p.publish_event(
-        "user_created".to_owned(),
         UserCreatedEventMessage {
             user_id: "4".to_owned(),
             user_name: "2406360256-Dira".to_owned(),
         },
-    );
-    _ = p.publish_event(
-        "user_created".to_owned(),
         UserCreatedEventMessage {
             user_id: "5".to_owned(),
             user_name: "2406360256-Emir".to_owned(),
         },
-    );
+    ] {
+        let payload = message.try_to_vec()?;
+
+        channel
+            .basic_publish(
+                "".into(),
+                "user_created".into(),
+                BasicPublishOptions::default(),
+                &payload,
+                BasicProperties::default(),
+            )
+            .await?
+            .await?;
+    }
+
+    Ok(())
 }
